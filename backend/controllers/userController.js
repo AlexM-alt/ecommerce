3dfc0d921301150cbfd7b20eb773eb5f7423e7c0
aleksander Micyk <aleksander.micyk@gmail.com>
2022-01-08 12:18:47 +0100

    add backend user routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,131 +1,251 @@
 const ErrorHandler = require("../utils/errorhander");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
-const User = require("../models/userModel")
-const sendToken = require("../utils/jwtToken")
-const sendEmail = require("../utils/sendEmail")
-const crypto = require("crypto")
+const User = require("../models/userModel");
+const sendToken = require("../utils/jwtToken");
+const sendEmail = require("../utils/sendEmail");
+const crypto = require("crypto");
 
 // Register a User
-exports.registerUser = catchAsyncErrors(async(req,res,next) => {
-    const {name, email, password} = req.body
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+  const { name, email, password } = req.body;
 
-    const user = await User.create({
-        name,email,password,
-        avatar:{
-            public_id:"this is a sample id",
-            url:"profilepicUrl"
-        }
-    })
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "this is a sample id",
+      url: "profilepicUrl",
+    },
+  });
 
-    sendToken(user, 201, res)
-})
+  sendToken(user, 201, res);
+});
 
 // Login User
-exports.loginUser = catchAsyncErrors(async(req,res,next) => {
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
 
-    const {email,password} = req.body
+  // checking if user has given password and email both
 
-    // checking if user has given password and email both
+  if (!email || !password) {
+    return next(new ErrorHandler("Please Enter Email & Password", 400));
+  }
 
-    if(!email || !password){
-        return next(new ErrorHandler("Please Enter Email & Password", 400))
-    }
+  const user = await User.findOne({ email }).select("+password");
 
-    const user = await User.findOne({email}).select("+password")
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
 
-    if(!user){
-        return next(new ErrorHandler("Invalid email or password", 401))
-    }
+  const isPasswordMatched = await user.comparePassword(password);
 
-    const isPasswordMatched = await user.comparePassword(password)
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
 
-    if(!isPasswordMatched){
-        return next(new ErrorHandler("Invalid email or password", 401))
-    }
-
-    sendToken(user, 200, res)
-})
+  sendToken(user, 200, res);
+});
 
 // Logout User
-exports.logout  = catchAsyncErrors(async (req,res,next) => {
+exports.logout = catchAsyncErrors(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
 
-    res.cookie("token", null, {
-        expires:new Date(Date.now()),
-        httpOnly: true
-    })
-
-    res.status(200).json({
-        success: true,
-        message: "Logged Out"
-    })
-})
-
+  res.status(200).json({
+    success: true,
+    message: "Logged Out",
+  });
+});
 
 // Forgot Password
-exports.forgotPassword = catchAsyncErrors(async(req,res,next) => {
-    const user = await User.findOne({email:req.body.email})
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
 
-    if(!user){
-        return next(new ErrorHandler("User not found", 404))
-    }
+  if (!user) {
+    return next(new ErrorHandler("User not found", 404));
+  }
 
-    // Get ResetPassword Token
-    const resetToken = user.getResetPasswordToken()
+  // Get ResetPassword Token
+  const resetToken = user.getResetPasswordToken();
 
-    await user.save({validateBeforeSave:false})
+  await user.save({ validateBeforeSave: false });
 
-    const resetPasswordUrl = `${req.protocol}://${req.get("host")}/api/v1/password/reset/${resetToken}`
+  const resetPasswordUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/password/reset/${resetToken}`;
 
-    const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\nIf you have not requested this email then please ignore it`
+  const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\nIf you have not requested this email then please ignore it`;
 
-    try{
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: `Ecommerce Password Recovery`,
+      message,
+    });
 
-        await sendEmail({
-            email:user.email,
-            subject:`Ecommerce Password Recovery`,
-            message,
-        })
+    res.status(200).json({
+      success: true,
+      message: `Email sent to ${user.email} successfully`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
 
-        res.status(200).json({
-            success:true,
-            message:`Email sent to ${user.email} successfully`
-        })
+    await user.save({ validateBeforeSave: false });
 
-    }catch (error){
-        user.resetPasswordToken = undefined
-        user.resetPasswordExpire = undefined
-
-        await user.save({validateBeforeSave:false})
-
-        return next(new ErrorHandler(error.message, 500))
-    }
-})
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
 
 // Reset Password
 exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
+  // creating token hash
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.token)
+    .digest("hex");
 
-    // creating token hash
-   const resetPasswordToken = crypto.createHash("sha256").update(req.params.token).digest("hex")
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
 
-    const user = await User.findOne({
-        resetPasswordToken,
-        resetPasswordExpire:{$gt:Date.now()},
-    })
+  if (!user) {
+    return next(
+      new ErrorHandler(
+        "Reset Password Token is invalid or has been expired",
+        400
+      )
+    );
+  }
 
-    if(!user){
-        return next(new ErrorHandler("Reset Password Token is invalid or has been expired", 400))
-    }
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(new ErrorHandler("Password does not password", 400));
+  }
 
-    if(req.body.password !== req.body.confirmPassword){
-        return next(new ErrorHandler("Password does not password", 400))
-    }
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
 
-    user.password = req.body.password
-    user.resetPasswordToken = undefined
-    user.resetPasswordExpire = undefined
+  await user.save();
 
+  sendToken(user, 200, res);
+});
 
-    await user.save()
+// Get User Detail
+exports.getUserDetails = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
 
-    sendToken(user, 200, res)
-})
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+// update User password
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select("+password");
+
+  const isPasswordMatched = await user.comparePassword(req.body.oldPassword);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHander("Old password is incorrect", 400));
+  }
+
+  if (req.body.newPassword !== req.body.confirmPassword) {
+    return next(new ErrorHander("password does not match", 400));
+  }
+
+  user.password = req.body.newPassword;
+
+  await user.save();
+
+  sendToken(user, 200, res);
+});
+
+// update User Profile
+exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  // We will add cloudinary later
+
+  const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+// Get all users (admin)
+exports.getAllUsers = catchAsyncErrors(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    success: true,
+    users,
+  });
+});
+
+// Get single user (admin)
+exports.getSingleUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(
+      new ErrorHandler(`User does not exist with id: ${req.params.id}`)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+// update User Role -- Admin
+exports.updateUserRole = catchAsyncErrors(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+    role: req.body.role,
+  };
+
+  const user = await User.findByIdAndUpdate(req.params.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+// Delete User -- Admin
+exports.deleteUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  // We will add cloudinary later
+
+  if (!user) {
+    return next(
+      new ErrorHandler(`User does not exit with id: ${req.params.id}`)
+    );
+  }
+
+  await user.remove();
+
+  res.status(200).json({
+    success: true,
+    message: "User Deleted successfully",
+  });
+});
--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,11 @@ const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
 
 // Handling Uncaught Exception
-process.on("uncaughtException",(err)=>{
-  console.log(`Error: ${err.message}`)
-  console.log(`Shutting down the server due to Uncaught Exception`)
-  process.exit(1)
-})
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
 
 // Config
 
@@ -21,13 +21,12 @@ const server = app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
 });
 
-
 // Unhandled Promise Rejection
-process.on('unhandledRejection', err=> {
-  console.log(`Error: ${err.message}`)
-  console.log(`Shutting down the server due Unhandled Promise Rejection`)
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due Unhandled Promise Rejection`);
 
   server.close(() => {
-    process.exit(1)
-  })
-})
+    process.exit(1);
+  });
+});
